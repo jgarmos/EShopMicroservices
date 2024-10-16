1353ce09287149d8760d962292791982b9600bb6
jgarmos <javiergarciaramos@gmail.com>
2024-10-16 18:40:56 +0200

    Addeed createProduct endpoint functionality using carter and Mapster

--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -1,9 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 //Configure the http request pipeline
+app.MapCarter();
+
 
 app.Run();
