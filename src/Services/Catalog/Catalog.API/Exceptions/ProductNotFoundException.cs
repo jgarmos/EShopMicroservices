264c3cee8692c6590e9ee4499ae9c11a079b4bd3
jgarmos <javiergarciaramos@gmail.com>
2024-10-28 22:42:41 +0100

    added Get productById Handler

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Catalog.API.Exceptions;
+
+public class ProductNotFoundException : Exception
+{
+    public ProductNotFoundException() : base("product not found")
+    {
+
+    }
+}
+
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,31 @@
+﻿
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.GetProductById;
+
+public record GetgProductByIdQuery(Guid id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(Product product);
+
+internal class GetProductByIdQueryHandler
+    (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+    : IQueryHandler<GetgProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+
+        logger.LogInformation("GetProductByIdQueryHanlder. Hnadle called with {@query}", query);
+
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if (prodcut is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByIdResult(product);
+
+    }
+
+}
+
